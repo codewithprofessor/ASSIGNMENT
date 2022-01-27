469569d97618822c460f30813238489f162a3343
Ali <92303718+codewithprofessor@users.noreply.github.com>
2022-01-27 19:59:06 +0000

    Update 10_Assignment_Question.cpp

--- 06ASSIGNMENT/10_Assignment_Question.cpp
+++ 06ASSIGNMENT/10_Assignment_Question.cpp
@@ -5,11 +5,48 @@ declares another structure Order that contains OrderID and an array of Book of l
  The program finally displays the values.
  */
 #include<iostream>
+#include<string>
+#include<iomanip>
 using namespace std;
+
+struct  book
+{
+    int book_id;
+    string book_name;
+    int price;
+};
+struct order
+{
+    int order_id;
+    book a[5];
+};
+
 
 int main()
 {
     // code by ali
-    // uploaded soon
+    order b;
+    cout<<"Enter Order id  : ";
+    cin>>b.order_id;
+    for(int i=0;i<5;i++)
+    {
+        cout<<"Enter the Book ID : ";
+        cin>>b.a[i].book_id;
+        cout<<"Enter The Name of Book : ";
+        cin.ignore();
+        getline(cin,b.a[i].book_name);
+        cout<<"Etner the Price of Book : ";
+        cin>>b.a[i].price;
+
+    }
+    cout<<"USER ID :"<<b.order_id<<endl;
+    cout<<setw(15)<<"BOOK NAME  "<<setw(8)<<"ID   "<<setw(7)<<"PRICE "<<endl;
+    for(int i=0;i<5;i++)
+    {
+        cout<<setw(9)<<b.a[i].book_name<<setw(8)<<b.a[i].book_id<<setw(7)<<b.a[i].price<<endl;
+    }
+
+    
     return 0;
 }
+
